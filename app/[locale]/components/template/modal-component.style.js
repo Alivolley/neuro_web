307b7d2453147c1f07f8e03fbf29debb389ca825
alivolley <alicryptovolley@gmail.com>
2023-08-17 12:13:48 +0330

    background of search modal debugged

--- app/[locale]/components/template/modal-component.style.js
+++ app/[locale]/components/template/modal-component.style.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { Dialog } from '@mui/material';
 
 export const ModalComponentStyle = styled(Dialog)(() => ({
-    '& .css-m9glnp-MuiPaper-root-MuiDialog-paper': {
+    '& div:first-child': {
         backgroundColor: 'transparent'
     }
 }));
